4eade5c90316f0d5bca494400f15f8184b5e23a0
rob1694 <robertb1694@gmail.com>
2021-04-06 13:16:58 -0400

    complete

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-
 // Write your code here
+const num1 = 2, num2 =31;
+const multiply = num1 * num2;
+const random = Math.ceil( Math.random() * 10 ) + 1;
+const num3 = 16, num4 = 6;
+const mod = num3 % num4;
+const max = Math.max( 1, 10, 15, 20 );
+console.log(random);
